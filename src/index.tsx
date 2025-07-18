55381e5d087b194cf30b2ed5424da876251cbdf3
d-exit <goodtoy.deguchi@gmail.com>
2025-07-18 16:40:39 +0900

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
+// src/index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// Service Workerの登録
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      })
-      .catch(error => {
-        console.log('ServiceWorker registration failed: ', error);
-      });
-  });
-}
+// ↓ Service Worker の登録は一旦オフにします。
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/service-worker.js')
+//       .then(registration => {
+//         console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//       })
+//       .catch(error => {
+//         console.log('ServiceWorker registration failed: ', error);
+//       });
+//   });
+// }
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
