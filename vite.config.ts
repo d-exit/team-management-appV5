f1b63ef17f0e262686508252161c6922c6419da8
d-exit <goodtoy.deguchi@gmail.com>
2025-07-18 15:59:31 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,30 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import react       from '@vitejs/plugin-react'
+import path        from 'path'
 
 export default defineConfig({
-  // 追加：プロジェクトのルートを src/ にする
+  // 「src/ がプロジェクトのルートだよ」と教えてあげる
   root: path.resolve(__dirname, 'src'),
+
+  // publicDir をプロジェクト直下の public/ に戻す
+  publicDir: path.resolve(__dirname, 'public'),
 
   plugins: [react()],
   resolve: {
     alias: {
-      '@':      path.resolve(__dirname, 'src'),
-      'utils':  path.resolve(__dirname, 'src/utils'),
-      'types':  path.resolve(__dirname, 'src/types.ts'),
+      '@':     path.resolve(__dirname, 'src'),
+      'utils': path.resolve(__dirname, 'src/utils'),
+      'types': path.resolve(__dirname, 'src/types.ts'),
     },
   },
   build: {
     // ビルド成果物をルート直下の dist/ に出力
     outDir: path.resolve(__dirname, 'dist'),
     emptyOutDir: true,
-  },
+    // ここで「src/index.html を入口として使ってね」と Rollup に教える
+    rollupOptions: {
+      input: path.resolve(__dirname, 'src/index.html')
+    }
+  }
 })
